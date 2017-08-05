e5c5dca5dc8833cd1f34d65ff86ad5e05b0ca378
Administrator <toei18022538@gmail.com>
2017-08-05 14:50:40 +0700

    before commit

---
+++ app/src/main/java/com/example/administrator/myrbac/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.administrator.myrbac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.myrbac.fragment.MainFragment;
+pho
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Add Fragment
+        if (savedInstanceState == null) {
+            MainFragment mainFragment = new MainFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.maiContainer,mainFragment)
+                    .commit();
+        }
+
+
+    }   // Main Method
+}   // Main Class
